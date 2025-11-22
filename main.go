c35c56a7f070db9ea4fc3ad9f74ea8826ac2b61b
aemet <nomvd@icloud.com>
2025-11-22 18:41:28 +0200

    home task 1

--- main.go
+++ main.go
@@ -1,36 +1,3 @@
 package main
 
-import "github.com/aemet93/GoGoGo/lessons"
-
-func main() {
-	//students.GetStudentsInfo()
-	//students.PrintOnlyEven(1, 55, 79, 35, 45, 65, 56, 75)
-	//lessons.Practice()
-
-	//var Robert lessons.Dog
-	//Robert.Name = "Robert"
-	//Robert.Color = "brown"
-	//
-	//Richard := lessons.Dog{
-	//	Name:  "Richard",
-	//	Color: "grey",
-	//}
-	//
-	//Richard.Run()
-	//Robert.Run()
-	//
-	//Richard.SayName()
-	//Robert.SayName()
-	//
-	//Dogs := []lessons.Dog{Robert, Richard}
-	//
-	//SayDogsName(Dogs)
-
-	portfolio.RunPortfolio()
-}
-
-func SayDogsName(dogs []lessons.Dog) {
-	for _, dog := range dogs {
-		dog.SayName()
-	}
-}
+func main() {}
